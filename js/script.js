fa3cf7375628b963aea169ca2aba874b1e97758c
regina-fonseca-avila <61661295+regina-fonseca-avila@users.noreply.github.com>
2020-08-03 15:19:22 -0300

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-170496777-1');
-    
 /* slider buttons */
 $(function() {
     try {
